eddb232da4f36f9888ae8fd90c18ac40575f3f04
Kyle <polysplitter@gmail.com>
2019-06-14 21:08:31 -0500

    filter ready

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+function filterData(data) {
+    return data.filter(d => {
+        return (
+            d.release_year > 1999 &&
+            d.release_year < 2010 &&
+            d.revenue > 0 &&
+            d.budget > 0 &&
+            d.genre &&
+            d.title
+        )
+    })
+}
+
+function ready(movies){
+    const moviesClean = filterData(movies)
+    console.log(moviesClean)
+}
+
 const parseNA = string => (string === 'NA' ? undefined : string)
 const parseDate = string => d3.timeParse('%Y-%m-%d')(string)
 
@@ -6,7 +24,7 @@ function type(d) {
     return {
         budget: +d.budget,
         genre: parseNA(d.genre),
-        genres: d.genres,
+        genres: JSON.parse(d.genres).map(d => d.name),
         homepage: parseNA(d.homepage),
         id: +d.id,
         imdb_id: parseNA(d.imdb_id),
@@ -14,8 +32,9 @@ function type(d) {
         overview: parseNA(d.overview),
         popularity: +d.popularity,
         poster_path: parseNA(d.poster_path),
-        production_countries: d.production_countries,
+        production_countries: JSON.parse(d.production_countries),
         release_date: parseDate(d.release_date),
+        release_year: parseDate(d.release_date).getFullYear(),
         revenue: +d.revenue,
         runtime: +d.runtime,
         tagline: parseNA(d.tagline),
@@ -27,5 +46,5 @@ function type(d) {
 
 // load data
 d3.csv('data/movies.csv', type).then(res => {
-    console.log(res)
+    ready(res)
 })
